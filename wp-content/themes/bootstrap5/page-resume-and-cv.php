91cf27bb3e6585ee4d4f16894f148f8a082961e0
Loc Tran <mrlocspoc@gmail.com>
2022-06-25 16:45:02 +0930

    150. Work Experience complete

--- wp-content/themes/bootstrap5/page-resume-and-cv.php
+++ wp-content/themes/bootstrap5/page-resume-and-cv.php
@@ -40,6 +40,95 @@ get_header(); ?>
                     </figure>
                 </div><!-- / card-body -->
             </div><!-- / card -->
+            <div class="card mb-4">
+                <div class="card-body">
+
+                    <div class="d-flex align-items-start mb-2">
+                        <i class="fab fa-facebook fa-fw fa-3x text-primary me-2"></i>
+                        <div class="flex-grow-1">
+                            <h3 class="h4 mb-1">Front-End Developer</h3>
+                            <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                <h5 class="mb-0"><a href="#0" class="text-decoration-none">Facebook</a></h5>
+                                <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                            </div><!-- / d-sm-flex -->
+                        </div><!-- / flex-grow-1 -->
+                    </div><!-- / d-flex -->
+                    <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                    <hr>
+
+                    <div class="d-flex align-items-start mb-2">
+                        <i class="fab fa-apple fa-fw fa-3x text-primary me-2"></i>
+                        <div class="flex-grow-1">
+                            <h3 class="h4 mb-1">Art Director</h3>
+                            <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                <h5 class="mb-0"><a href="#0" class="text-decoration-none">Apple</a></h5>
+                                <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                            </div><!-- / d-sm-flex -->
+                        </div><!-- / flex-grow-1 -->
+                    </div><!-- / d-flex -->
+                    <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                    <hr>
+
+                    <div class="d-flex align-items-start mb-2">
+                        <i class="fab fa-dropbox fa-fw fa-3x text-primary me-2"></i>
+                        <div class="flex-grow-1">
+                            <h3 class="h4 mb-1">User Experience Designer</h3>
+                            <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                <h5 class="mb-0"><a href="#0" class="text-decoration-none">Dropbox</a></h5>
+                                <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                            </div><!-- / d-sm-flex -->
+                        </div><!-- / flex-grow-1 -->
+                    </div><!-- / d-flex -->
+                    <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                    <hr>
+
+                    <div class="collapse" id="collapse-work">
+
+                        <div class="d-flex align-items-start mb-2">
+                            <i class="fab fa-medium fa-fw fa-3x text-primary me-2"></i>
+                            <div class="flex-grow-1">
+                                <h3 class="h4 mb-1">Copywriter</h3>
+                                <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                    <h5 class="mb-0"><a href="#0" class="text-decoration-none">Medium</a></h5>
+                                    <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                                </div><!-- / d-sm-flex -->
+                            </div><!-- / flex-grow-1 -->
+                        </div><!-- / d-flex -->
+                        <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                        <hr>
+
+                        <div class="d-flex align-items-start mb-2">
+                            <i class="fab fa-google fa-fw fa-3x text-primary me-2"></i>
+                            <div class="flex-grow-1">
+                                <h3 class="h4 mb-1">Search Engine Consultant</h3>
+                                <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                    <h5 class="mb-0"><a href="#0" class="text-decoration-none">Google</a></h5>
+                                    <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                                </div><!-- / d-sm-flex -->
+                            </div><!-- / flex-grow-1 -->
+                        </div><!-- / d-flex -->
+                        <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                        <hr>
+
+                        <div class="d-flex align-items-start mb-2">
+                            <i class="fab fa-uber fa-fw fa-3x text-primary me-2"></i>
+                            <div class="flex-grow-1">
+                                <h3 class="h4 mb-1">AI Engineer</h3>
+                                <div class="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+                                    <h5 class="mb-0"><a href="#0" class="text-decoration-none">Uber</a></h5>
+                                    <small class="text-uppercase fw-bold text-muted"><time datetime="2020-01">January 2020</time> &ndash; <time datetime="2020-12">December 2020</time></small>
+                                </div><!-- / d-sm-flex -->
+                            </div><!-- / flex-grow-1 -->
+                        </div><!-- / d-flex -->
+                        <p class="card-text">Pug succulents selvage, umami mixtape kinfolk dreamcatcher marfa.</p>
+                        <hr>
+
+                    </div><!-- / collapse-work -->
+
+                    <button type="button" class="btn btn-sm btn-outline-primary" data-bs-toggle="collapse" data-bs-target="#collapse-work" aria-expanded="false" aria-controls="collapse-work">Show all</button>
+
+                </div><!-- / card-body -->
+            </div><!-- / card -->
         </div><!-- / col-12 -->
     </div><!-- / row -->
 </div><!-- / container -->
