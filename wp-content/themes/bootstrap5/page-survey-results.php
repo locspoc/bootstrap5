b76e1b1982c4fc18b678c5ca23f87c6a0fa31298
Loc Tran <mrlocspoc@gmail.com>
2022-07-24 18:17:22 +0930

    188. Results - Percentages complete

--- wp-content/themes/bootstrap5/page-survey-results.php
+++ wp-content/themes/bootstrap5/page-survey-results.php
@@ -14,7 +14,263 @@ if (!defined('ABSPATH')) {
 
 get_header(); ?>
 
+<body class="bg-light py-3 py-lg-5">
 
+    <div class="container">
+
+        <div class="text-center">
+
+            <h1 class="display-4">Survey Name</h1>
+            <p class="lead">Subtitle for the Survey Name</p>
+
+        </div><!-- / text-center -->
+
+        <div class="mb-5">
+
+            <h2 class="h3 text-center mb-4">Results with percentages</h2>
+            <h3 class="h5 mb-3">Question #1: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h3>
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+                    <p class="mb-0">Answer for radio 1</p>
+                </div>
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 12%;" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100">12%</div>
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+                    <p class="mb-0">Answer for radio 2</p>
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 17%;" aria-valuenow="17" aria-valuemin="0" aria-valuemax="100">17%</div>
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+                    <p class="mb-0">Answer for radio 3</p>
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 42%;" aria-valuenow="42" aria-valuemin="0" aria-valuemax="100">42%</div>
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 4</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 5</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: 9%;" aria-valuenow="9" aria-valuemin="0" aria-valuemax="100">9%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <hr class="my-4">
+
+            <h3 class="h5 mb-3">Question #2: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h3>
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 1</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 24%;" aria-valuenow="24" aria-valuemin="0" aria-valuemax="100">24%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 2</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 47%;" aria-valuenow="47" aria-valuemin="0" aria-valuemax="100">47%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 3</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 19%;" aria-valuenow="19" aria-valuemin="0" aria-valuemax="100">19%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 4</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: 10%;" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <hr class="my-4">
+
+            <h3 class="h5 mb-3">Question #3: Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h3>
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 1</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-danger" role="progressbar" style="width: 14%;" aria-valuenow="14" aria-valuemin="0" aria-valuemax="100">14%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 2</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-info" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+            <div class="row mb-2">
+
+                <div class="col-md-6">
+
+                    <p class="mb-0">Answer for radio 3</p>
+
+                </div><!-- / col-md-6 -->
+
+                <div class="col-md-6">
+
+                    <div class="progress">
+
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 61%;" aria-valuenow="61" aria-valuemin="0" aria-valuemax="100">61%</div>
+
+                    </div><!-- / progress -->
+
+                </div><!-- / col-md-6 -->
+
+            </div><!-- / row -->
+
+        </div><!-- / mb-5 -->
+
+    </div><!-- / container -->
+
+</body>
 
 <?php kadence()->print_styles('kadence-content');
 /**
