c1cb5f7276b679b4fed3cd499f3ac893aae91056
Loc Tran <mrlocspoc@gmail.com>
2022-08-07 11:26:30 +0930

    208. Home - Navigation complete

---
+++ wp-content/themes/bootstrap5/page-small-business-about.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Template Name: Small Business About
+ *
+ * @package kadence
+ */
+
+namespace Kadence;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header(); ?>
+
+<style>
+    body {
+        /* padding-top: 57px; */
+    }
+</style>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom fixed-top">
+        <div class="container">
+            <a href="<?= home_url('small-business-home'); ?>" class="navbar-brand">Small Business</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a href="<?= home_url('small-business-home'); ?>" class="nav-link active" aria-current="page">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= home_url('small-business-home/small-business-services'); ?>" class="nav-link">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= home_url('small-business-home/small-business-about'); ?>" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= home_url('small-business-home/small-business-team'); ?>" class="nav-link">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= home_url('small-business-home/small-business-contact'); ?>" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+                <div class="dropdown">
+                    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownLanguage" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Change language:</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownLanguage">
+                        <a href="https://companyname.com/language" class="dropdown-item">Language</a>
+                        <a href="https://companyname.com/language" class="dropdown-item">Language</a>
+                        <a href="https://companyname.com/language" class="dropdown-item">Language</a>
+                    </div><!-- / dropdown-menu -->
+                </div><!-- / dropdown -->
+            </div><!-- / collapse -->
+        </div><!-- / container -->
+    </nav>
+</body>
+
+<?php kadence()->print_styles('kadence-content');
+/**
+ * Hook for everything, makes for better elementor theming support.
+ */
+// do_action( 'kadence_single' );
+
+get_footer();
